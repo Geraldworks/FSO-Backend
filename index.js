df3525f9000ed651e8922a46cadd952030b3f2cb
Gerald Ho <97504394+Geraldworks@users.noreply.github.com>
2024-03-02 22:32:57 +0800

    completed 3.15 - 3.18

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 // This will require the module from the file,
 const PhoneNumber = require("./models/phoneNumber");
+const phoneNumber = require("./models/phoneNumber");
 
 const app = express();
 
@@ -35,29 +36,6 @@ app.use(cors());
 app.use(express.json());
 app.use(customLogger);
 
-// let persons = [
-//   {
-//     id: 1,
-//     name: "Arto Hellas",
-//     number: "040-123456",
-//   },
-//   {
-//     id: 2,
-//     name: "Ada Lovelace",
-//     number: "39-44-5323523",
-//   },
-//   {
-//     id: 3,
-//     name: "Dan Abramov",
-//     number: "12-43-234345",
-//   },
-//   {
-//     id: 4,
-//     name: "Mary Poppendieck",
-//     number: "39-23-6423122",
-//   },
-// ];
-
 // GET info
 
 app.get("/info", (request, response) => {
@@ -78,15 +56,11 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-// GET single Person (TBC)
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+// GET single Person
+app.get("/api/persons/:id", (request, response, next) => {
+  PhoneNumber.findById(request.params.id)
+    .then((phoneNumber) => response.json(phoneNumber))
+    .catch((error) => next(error));
 });
 
 // POST single Person
@@ -102,27 +76,44 @@ app.post("/api/persons", (request, response) => {
     return response.status(400).json({
       error: "number missing",
     });
-  } 
-  // else if (persons.find((person) => person.name === content.name)) {
-  //   return response.status(400).json({
-  //     error: "name must be unique",
-  //   });
-  // }
+  }
+  else if (persons.find((person) => person.name === content.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
 
   const person = new PhoneNumber({
     name: content.name,
     number: content.number,
   });
   person.save().then((savedPerson) => {
-    response.json(savedPerson)
-  })
+    response.json(savedPerson);
+  });
 });
 
-// DELETE single Person (TBC)
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  console.log(body)
+  const phoneNumber = {
+    name: body.name,
+    number: body.number,
+  };
+
+  PhoneNumber.findByIdAndUpdate(request.params.id, phoneNumber, { new: true })
+    .then((updatedPhoneNumber) => {
+      response.json(updatedPhoneNumber);
+    })
+    .catch((error) => next(error));
+});
+
+// DELETE single Person
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
+  PhoneNumber.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 // Catch all
@@ -131,6 +122,18 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
